28f9bea70cc7b528b5164e8ddb69defca960b7c7
TomasMachado <tomas.machado@uniriotec.br>
2018-11-21 03:10:26 -0200

    Criada tela de acesso negado e moduralizados componentes html

--- app/routes/web2.js
+++ app/routes/web2.js
@@ -1,6 +1,8 @@
 var loginController = require('../controllers/loginController');
 var devolucaoController = require('../controllers/devolucaoController');
 var session = require('express-session');
+var express = require('express');
+var path = require('path');
 
 
 var sess;
@@ -12,6 +14,29 @@ module.exports = function(app) {
   app.post('/coisar', function (req, res) {
     devolucaoController.coisar(req, res);
   });
+
+  app.get('/devolucao/:id', function (req, res) {
+   var format = req.params.id;
+   console.log(format);
+   console.log(__dirname);
+   if(!req.session.cpf){
+      res.redirect('../acesso-negado');
+   }else
+   if(req.session.id_nivel == 0){
+     //mostrar pagina
+   }else{
+     res.redirect('../acesso-negado', {layout:false});
+
+   }
+
+});
+
+  app.get('/acesso-negado', function(req, res){
+    res.render('Tela_Principal/acesso_negado', {layout:false});
+
+  });
+
+  app.use(express.static('devolucao/public'));
 
   app.get('/logout',function(req,res){
     req.session.destroy(function(err) {
